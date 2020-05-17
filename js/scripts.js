000db540145694d61778d39bde03141024008bb7
gkarumbi <gkarumbi@gmail.com>
2020-05-17 10:15:37 +0300

    Created pizza object & Access it properties, It Works!

--- js/scripts.js
+++ js/scripts.js
@@ -2,19 +2,34 @@
 //Create  Objects and their properties
 
 var sizes ={ small: 500,
-    medium = 650,
-    Large = 800
+    medium : 650,
+    large : 800
 };
 
 var toppings = {
     ham: 50,
-    olives = 100,
-    cheese = 150
+    olives : 100,
+    cheese : 150
 };
 
 var crust ={
-    crusty = 80,
-    stuffed = 140,
-    gluttenFree = 200
+    crispy : 80,
+    stuffed :140,
+    gluttenFree : 200
 };
+
+//create Pizza Object and add its properies
+class Pizza {
+    constructor(pizzaSizes, pizzaToppings, pizzaCrust) {
+        this.pizzaSizes = pizzaSizes;
+        this.pizzaToppings = pizzaToppings;
+        this.pizzaCrust = pizzaCrust;
+    }
+}
+
+var newPizza = new Pizza(sizes,toppings,crust);
+
+console.log(newPizza.pizzaSizes.small);
+console.log(newPizza.pizzaToppings.cheese);
+console.log(newPizza.pizzaCrust.crispy);
 

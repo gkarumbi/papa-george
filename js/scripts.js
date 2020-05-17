97d4061e1aeae1d7933a105fbc6c5f835abf13aa
gkarumbi <gkarumbi@gmail.com>
2020-05-17 12:04:37 +0300

    Added radio button functionality

--- js/scripts.js
+++ js/scripts.js
@@ -33,3 +33,46 @@ console.log(newPizza.pizzaSizes.small);
 console.log(newPizza.pizzaToppings.cheese);
 console.log(newPizza.pizzaCrust.crispy);
 
+/* class UI{
+    constructor(){
+        this.pizzaContainer = document.getElement
+    }
+} */
+
+//Front-end Logic
+
+$(document).ready(function(){
+    $('.pizza-container').click(function(event){
+        /* if(event.cur){
+
+        } */
+        /* var target =$(event.target);
+        if(target.is('input')){
+            console.log(target.val());
+
+        } */
+        $("input[type='radio']").click(function(){
+            var pizzaradioValue =  $("input[name='pizza']:checked").val();
+            console.log(pizzaradioValue);
+            if(pizzaradioValue == "smallpizza"){
+
+            } else if(pizzaradioValue == "mediumpizza"){
+
+            } else if(pizzaradioValue =="largepizza"){
+
+            }
+        });
+        $("input[type='radio']").click(function(){
+            var crustradioValue =  $("input[name='crust']:checked").val();
+            console.log(crustradioValue);
+        });
+        $("input[type='checkbox']").click(function(){
+            var toppingsradioValue =  $("input[name='toppings']:checked").val();
+            console.log(toppingsradioValue);
+        });
+             
+             
+                
+       
+    });
+});
